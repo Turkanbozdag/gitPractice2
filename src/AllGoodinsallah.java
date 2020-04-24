be82979eb4cd0bc8c05edb9fef79ca67e9abf422
TurkanBozdag <turkanbozdag@yahoo.com>
2020-04-24 16:51:13 -0400

    first

--- src/AllGoodinsallah.java
+++ src/AllGoodinsallah.java
@@ -1,5 +1,7 @@
 public class AllGoodinsallah {
     public static void main(String[] args) {
         //I hope everything will be good after these courses
+
+        System.out.println("Hello World");
     }
 }
